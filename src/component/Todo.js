844ffda328d5c2cd8785d0e984d93db44bb1c98c
Amanshaw9010 <109677596+Amanshaw9010@users.noreply.github.com>
2022-10-12 23:17:46 +0530

    first commit

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addTodo } from '../action/Index';
+import todoReducer from '../reducer/TodoReducer';
+
+
 
 
 const Todo = () => {
   const [inputData, setInputData] = useState(" ");
   const dispatch = useDispatch();
-  const [display, setDisplay] = useState(" ");
+
+  const list = useSelector((state) =>
+    state.todoReducer.list
+  )
+
+  console.log(list);
+
+
 
 
   return (
@@ -18,8 +28,16 @@ const Todo = () => {
             value={inputData}
             onChange={(event) => setInputData(event.target.value)}
           />
-          <button onClick={() => { dispatch(addTodo(inputData), setInputData(" ") , setDisplay(inputData)  ) }}>+</button>
-          <h1>{display}</h1>
+          <button onClick={() => { dispatch(addTodo(inputData), setInputData(" ")) }}>+</button>
+          {
+            list.map((elem) => {
+              return (
+                <div className='shoeItems' key={elem.id}>
+                  <h3>{elem.data}</h3>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </>
